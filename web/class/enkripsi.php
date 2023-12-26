181cc6a3554a11063de05f4348bc31df038804e3
Yulianto <139340153+yuliantoyu@users.noreply.github.com>
2023-12-26 09:25:07 +0700

    Update enkripsi.php

--- web/class/enkripsi.php
+++ web/class/enkripsi.php
@@ -8,13 +8,13 @@ class Enkripsi
         return openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
     }
 
-    // Fungsi untuk menghasilkan KEY (kunci) dari passphrase dengan menggunakan PBKDF2
+ 
     private static function generateKey($passphrase, $salt)
     {
         return hash_pbkdf2('sha256', $passphrase, $salt, 1000, 32, true);
     }
 
-    // Fungsi untuk melakukan enkripsi menggunakan OpenSSL
+  
     public static function encrypt($data, $passphrase)
     {
         $salt = self::generateIV(); // Menghasilkan IV sebagai salt
@@ -22,18 +22,18 @@ class Enkripsi
 
         $encrypted = openssl_encrypt($data, 'aes-256-cbc', $key, 0, $salt);
 
-        // Menggabungkan IV dan teks terenkripsi menjadi satu string
+      
         $result = base64_encode($salt . $encrypted);
 
         return $result;
     }
 
-    // Fungsi untuk melakukan dekripsi menggunakan OpenSSL
+   
     public static function decrypt($data, $passphrase)
     {
         $data = base64_decode($data);
 
-        // Mengambil IV dari data terenkripsi
+     
         $salt = substr($data, 0, 16);
         $data = substr($data, 16);
 
@@ -45,16 +45,16 @@ class Enkripsi
     }
 }
 
-// Contoh penggunaan
+
 $originalText = "Ini adalah teks yang akan dienkripsi";
 
-$passphrase = "kunciRahasia123"; // Ganti passphrase dengan yang lebih aman di lingkungan produksi
+$passphrase = "kunciRahasia123"; 
 
-// Enkripsi
+
 $encryptedText = Enkripsi::encrypt($originalText, $passphrase);
 echo "Teks Enkripsi: $encryptedText\n";
 
-// Dekripsi
+
 $decryptedText = Enkripsi::decrypt($encryptedText, $passphrase);
 echo "Teks Dekripsi: $decryptedText\n";
 
